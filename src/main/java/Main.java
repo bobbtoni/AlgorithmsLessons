96b0a50b99d68f817f18cbc9b165f22cb96a2840
bobbtoni <bobb.toni.phone@gmail.com>
2020-01-02 06:49:34 +0700

    Добавлена функция, которая переворачивает вводимые строки.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
         Deck<Integer> deck = new Deck<Integer>();
@@ -10,5 +12,20 @@ public class Main {
             System.out.println(deck.removeFront());
         }
 
+        String string = "123";
+        System.out.println(reverse(string));
+
+    }
+
+    public static String reverse(String string){
+        StringBuilder stringBuilder = new StringBuilder();
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < string.length(); i++) {
+            stack.push(string.charAt(i));
+        }
+        while (!stack.isEmpty()){
+            stringBuilder.append(stack.pop());
+        }
+        return stringBuilder.toString();
     }
 }
