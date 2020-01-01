3a8c64689587a1f25adf6653b8c3e77451183909
bobbtoni <bobb.toni.phone@gmail.com>
2020-01-02 06:34:35 +0700

    Добавлен класс для реализации дека.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.insertFront(1);
+        deck.insertFront(2);
+        deck.insertFront(3);
+
+        while (!deck.isEmpty()){
+            System.out.println(deck.removeFront());
+        }
+
     }
 }
